4089aa1134c3a2cc91b156ded033f39192f69e73
lishifeng <lishifenging@outlook.com>
2025-04-18 23:38:29 +0800

    fix: fix doc errors

--- doc/mainpage/mainpage.h
+++ doc/mainpage/mainpage.h
@@ -1,10 +1,10 @@
 /**
  * @mainpage  LibDriver PCF8591
- * @brief     LibDriver PCF8591 is a full function driver of PCF8591 and it can run in both MCU and Linux platform.
+ * @brief     LibDriver PCF8591 is a full-featured driver of PCF8591 and it can run in both MCU and Linux platform.
  * @version   2.0.0
  * @author    Shifeng Li
  *
- * <h3>history</h3>
+ * <h3>History</h3>
  * <table>
  * <tr><th>Date        <th>Version  <th>Author      <th>Description
  * <tr><td>2021/02/18  <td>2.0      <td>Shifeng Li  <td>format the code
